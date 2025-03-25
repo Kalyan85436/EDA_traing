064a4e67f59be487f4ee51351b066cc8fbf3b68b
kalyan <kalyankumar8284@gmail.com>
2025-03-25 20:03:11 +0530

    Day1

--- truth_table.py
+++ truth_table.py
@@ -7,4 +7,19 @@ def or_gate(a,b):
 def not_gate(a):
     return int(not a)
 
+def truth_table(name,fun):
+        print(f"Truth table: {name} ")
+        print("X  Y  O/P")
+        print("-----------")
+        for i in range(2):
+            a = i
+            for j in range(2):
+                b = j
+                print(f"{a}  {j}  {fun(a,b)}")
 
+d ={
+    "AND":and_gate,
+    "OR":or_gate
+}
+for name,fun in d.items():
+    truth_table(name,fun)
