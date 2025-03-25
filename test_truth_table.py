69c23938c445fc193db4f287fa610f3c92b9700f
kalyan <kalyankumar8284@gmail.com>
2025-03-25 21:29:00 +0530

    Day1

--- test_truth_table.py
+++ test_truth_table.py
@@ -1,34 +1,18 @@
-from truth_table import and_1,or_1,not_1,xor_1,nand_1,nor_1,xnor_1
+from truth_table import*
 import pytest
-@pytest.mark.parametrize("a,b,c",[(0,0,0),(0,1,0),(1,0,0),(1,1,1)])
-def test_and(a,b,c):
-    assert and_1(a,b)==c
 
-@pytest.mark.parametrize("a,b,c",[(0,0,0),(0,1,1),(1,0,1),(1,1,1)])
-def test_or(a,b,c):
-    assert or_1(a,b)==c
+def test_and():
+    assert and_gate(0,0) == 0
+    assert and_gate(0,1) == 0
+    assert and_gate(1, 0) == 0
+    assert and_gate(1, 1) == 1
+
+def test_or():
+    assert or_gate(0,0) == 0
+    assert or_gate(0,1) == 1
+    assert or_gate(1, 0) == 1
+    assert or_gate(1, 1) == 1
 
 def test_not():
-    assert not_1(0)==1
-    assert not_1(1)==0
-
-@pytest.mark.parametrize("a,b,c",[(0,0,1),(0,1,1),(1,0,1),(1,1,0)])
-def test_or(a,b,c):
-    assert nand_1(a,b)==c
-
-@pytest.mark.parametrize("a,b,c",[(0,0,1),(0,1,0),(1,0,0),(1,1,0)])
-def test_or(a,b,c):
-    assert nor_1(a,b)==c
-
-@pytest.mark.parametrize("a,b,c",[(0,0,0),(0,1,1),(1,0,1),(1,1,0)])
-def test_or(a,b,c):
-    assert xor_1(a,b)==c
-
-@pytest.mark.parametrize("a,b,c",[(0,0,1),(0,1,0),(1,0,0),(1,1,1)])
-def test_or(a,b,c):
-    assert xnor_1(a,b)==c
-
-
-
-
-    print("hi")
+    assert not_gate(0) == 1
+    assert  not_gate(1) == 0
